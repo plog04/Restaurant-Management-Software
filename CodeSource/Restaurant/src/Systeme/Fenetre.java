501023b7c7c95774a46978d820a8fbb214486ca7
Guillaume <plog04@uqo.ca>
2012-12-04 08:28:43 -0500

    modification Fenetre et delete Chemin

--- CodeSource/Restaurant/src/Systeme/Fenetre.java
+++ CodeSource/Restaurant/src/Systeme/Fenetre.java
@@ -1,22 +1,38 @@
+package Systeme;
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
-public class Fenetre extends JFrame{
+public class Fenetre extends JFrame {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	//protected Container cFenetre;
 	protected Container cFenetre;
+	protected JPanel pRetour = new JPanel();
 	//protected BorderLayout layout = new BorderLayout();
+	protected JButton bRetour = new JButton("Retour aux choix de compte");
 	
 	public Fenetre(){
-		this.setSize(1000, 1000);
+		this.setSize(1000, 500);
 		this.setLocationRelativeTo(null);
 		this.setResizable(true);
+		pRetour.setLayout(new BorderLayout());
+		pRetour.add(bRetour,BorderLayout.SOUTH);
 		cFenetre = getContentPane();
-		//cFenetre.setLayout(layout);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
+		cFenetre.setLayout(new BorderLayout());
+		cFenetre.add(pRetour, BorderLayout.SOUTH);
+		
+		pRetour.setVisible(false);
 	}
 
 }
